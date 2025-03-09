30ef47bd0ee15236bd6e159b4bd458be9aa0f384
nazsmolic575 <nazsmolic575@gmail.com>
2025-03-09 08:14:19 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,18 @@
+// Random Math Homework Problems
+const mathHomework = {
+  problem1: () => {
+    const x = 2 + 3;
+    console.log(x);
+    return x;
+  },
+  problem2: () => {
+    const y = 5 - 2;
+    console.log(y);
+    return y;
+  },
+  problem3: () => {
+    const z = 10 * 2;
+    console.log(z);
+    return z;
+  },
+};
