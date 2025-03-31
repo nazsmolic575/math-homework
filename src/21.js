e2b322e695e9e1546cf86c16889b93bcbcb0b0f3
nazsmolic575 <nazsmolic575@gmail.com>
2025-03-31 03:22:31 +0000

    Add 21.js

---
+++ src/21.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+const randomNumber = getRandomInt(10); // Generate a random number between 0 and 9
+console.log(`The random number is: ${randomNumber}`);
