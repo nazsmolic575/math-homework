ffe180b9554f44e6539d116893b4d81ef716bb87
nazsmolic575 <nazsmolic575@gmail.com>
2025-04-06 12:49:33 +0000

    Add 26.js

---
+++ src/26.js
@@ -0,0 +1,20 @@
+function generateRandomString(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+function checkPalindrome(str) {
+  let left = 0, right = str.length - 1;
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
